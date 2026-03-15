b49f2e2e647862e5f057160c0c8d5192948fb90d
shinhanseo <imkara1@naver.com>
2026-03-15 23:23:13 +0900

    Feat : 로그인 시 User Model 생성

---
+++ frontend/lib/features/auth/models/login_response_model.dart
@@ -0,0 +1,45 @@
+class LoginResponseModel {
+  final String accessToken;
+  final String refreshToken;
+  final UserModel user;
+
+  LoginResponseModel({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.user,
+  });
+
+  factory LoginResponseModel.fromJson(Map<String, dynamic> json) {
+    return LoginResponseModel(
+      accessToken: json['access_token'],
+      refreshToken: json['refresh_token'],
+      user: UserModel.fromJson(json['user']),
+    );
+  }
+}
+
+class UserModel {
+  final int id;
+  final String? nickname;
+  final String? gender;
+  final String? ageRange;
+  final bool profileCompleted;
+
+  UserModel({
+    required this.id,
+    required this.nickname,
+    required this.gender,
+    required this.ageRange,
+    required this.profileCompleted,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      nickname: json['nickname'],
+      gender: json['gender'],
+      ageRange: json['age_range'],
+      profileCompleted: json['profile_completed'],
+    );
+  }
+}
