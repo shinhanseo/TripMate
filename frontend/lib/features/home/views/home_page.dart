697d27771510be4dc1fa67440656450c86dd59e4
shinhanseo <imkara1@naver.com>
2026-03-16 16:54:06 +0900

    Chore : splash 분기를 위한 홈화면 추가

---
+++ frontend/lib/features/home/views/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('홈')),
+      body: const Center(child: Text('홈화면입니다')),
+    );
+  }
+}
