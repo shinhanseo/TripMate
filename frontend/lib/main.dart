021cc1d6115425fe615a4b22e1ae1e9fcdb4618f
shinhanseo <imkara1@naver.com>
2026-03-16 15:32:35 +0900

    Chore : 전역 상태 관리 추가에 따른 main에 등록

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'features/auth/viewmodels/auth_state.dart';
 import 'app/app.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(create: (_) => AuthState(), child: const App()),
+  );
 }
