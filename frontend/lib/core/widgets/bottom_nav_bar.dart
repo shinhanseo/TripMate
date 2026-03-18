0d1530eceb56ee9b77979a83c3b4d72b1ab01151
shinhanseo <imkara1@naver.com>
2026-03-18 22:27:16 +0900

    Feat : 공통 하단바 추가

---
+++ frontend/lib/core/widgets/bottom_nav_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+
+  const BottomNavBar({super.key, required this.currentIndex});
+
+  void _onTap(BuildContext context, int index) {
+    if (index == currentIndex) return;
+
+    switch (index) {
+      case 0:
+        Navigator.pushReplacementNamed(context, '/chat');
+        break;
+      case 1:
+        Navigator.pushReplacementNamed(context, '/home');
+        break;
+      case 2:
+        Navigator.pushReplacementNamed(context, '/mypage');
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (index) => _onTap(context, index),
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: Color(0xff7ED6C2),
+      unselectedItemColor: Colors.grey,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.chat_bubble_rounded),
+          label: '채팅',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: '홈'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: '마이페이지',
+        ),
+      ],
+    );
+  }
+}
