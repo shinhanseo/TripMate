d22cbdcd221dcd61bc5cbe48f8a08d81a692d482
shinhanseo <imkara1@naver.com>
2026-03-27 00:25:54 +0900

    Feat : mypage view model 추가

---
+++ frontend/lib/features/mypage/viewmodels/mypage_viewmodel.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import '../models/mypage_model.dart';
+import '../services/mypage_api.dart';
+
+class MyPageViewModel extends ChangeNotifier {
+  final MyPageApi myPageApi;
+
+  MyPageViewModel({required this.myPageApi});
+
+  MyPageModel? myInfo;
+  bool isLoading = false;
+  String? errorMessage;
+  bool isSuccess = false;
+
+  Future<void> getMe() async {
+    try {
+      if (isLoading) return;
+
+      isLoading = true;
+      errorMessage = null;
+      notifyListeners();
+
+      final result = await myPageApi.getMe();
+
+      myInfo = result;
+      isSuccess = true;
+    } catch (e) {
+      errorMessage = e.toString().replaceFirst('Exception: ', '');
+      isSuccess = false;
+      myInfo = null;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
