330be53f5d0dc46a76f35361d0b0b89d59ce567c
shinhanseo <imkara1@naver.com>
2026-03-30 13:03:13 +0900

    Feat : 마이페이지 수정 view model 추가

---
+++ frontend/lib/features/mypage/viewmodels/profile_edit_viewmodel.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/foundation.dart';
+import '../services/mypage_api.dart';
+import '../models/profile_edit_model.dart';
+
+class ProfileEditViewModel extends ChangeNotifier {
+  final MyPageApi myPageApi;
+
+  ProfileEditViewModel({required this.myPageApi});
+
+  bool isLoading = false;
+  String? errorMessage;
+  bool isSuccess = false;
+
+  Future<void> editUser(ProfileEditModel edit) async {
+    try {
+      isLoading = true;
+      errorMessage = null;
+      isSuccess = false;
+      notifyListeners();
+
+      await myPageApi.editUser(edit: edit);
+      isSuccess = true;
+    } catch (e) {
+      errorMessage = e.toString().replaceFirst('Exception: ', '');
+      isSuccess = false;
+      rethrow;
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
