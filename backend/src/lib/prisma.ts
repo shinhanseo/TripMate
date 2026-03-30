806626479c25b6994b07dc50f16428bbf84821d2
shinhanseo <imkara1@naver.com>
2026-03-30 16:04:17 +0900

    Fix : Prsima7에 맞는 코드로 수정

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,3 +1,20 @@
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const pool = new Pool({
+  connectionString,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+  adapter,
+});
