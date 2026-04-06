66d16bce4f7f6c727a94225fc7511936c43402f4
shinhanseo <imkara1@naver.com>
2026-04-06 15:09:56 +0900

    Feat : 기존 chat 모델에 마지막 메시지관련 추가

--- frontend/lib/features/chat/models/chat_list_model.dart
+++ frontend/lib/features/chat/models/chat_list_model.dart
@@ -8,6 +8,11 @@ class ChatListModel {
   final DateTime chatJoinedAt;
   final DateTime roomCreatedAt;
   final DateTime roomUpdatedAt;
+  final int? lastMessageId;
+  final int? lastMessageSenderId;
+  final String? lastMessageSenderNickname;
+  final String? lastMessageContent;
+  final DateTime? lastMessageCreatedAt;
 
   ChatListModel({
     required this.roomId,
@@ -19,6 +24,11 @@ class ChatListModel {
     required this.chatJoinedAt,
     required this.roomCreatedAt,
     required this.roomUpdatedAt,
+    this.lastMessageId,
+    this.lastMessageSenderId,
+    this.lastMessageSenderNickname,
+    this.lastMessageContent,
+    this.lastMessageCreatedAt,
   });
 
   factory ChatListModel.fromJson(Map<String, dynamic> json) {
@@ -32,6 +42,17 @@ class ChatListModel {
       chatJoinedAt: DateTime.parse(json['chatJoinedAt'] as String),
       roomCreatedAt: DateTime.parse(json['roomCreatedAt'] as String),
       roomUpdatedAt: DateTime.parse(json['roomUpdatedAt'] as String),
+      lastMessageId: json['lastMessageId'] == null
+          ? null
+          : int.parse(json['lastMessageId'].toString()),
+      lastMessageSenderId: json['lastMessageSenderId'] == null
+          ? null
+          : int.parse(json['lastMessageSenderId'].toString()),
+      lastMessageSenderNickname: json['lastMessageSenderNickname'] as String?,
+      lastMessageContent: json['lastMessageContent'] as String?,
+      lastMessageCreatedAt: json['lastMessageCreatedAt'] == null
+          ? null
+          : DateTime.parse(json['lastMessageCreatedAt'] as String),
     );
   }
 }
